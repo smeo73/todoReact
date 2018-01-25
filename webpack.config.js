f648e80594f9755c216c42d4c86ac7847314702a
vesin philippe <pvesin@sapiens.biz>
2018-01-25 15:38:42 +0100

    add-encore

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,36 @@
-var config = {
-   entry: './index.js',
+let Encore  = require('@symfony/webpack-encore');
+let path    = require('path');
 
-   output: {
-      path:'/',
-      filename: 'bundle.js',
-   },
+const assets_path   = path.resolve('./');
+const output_path   = path.resolve('./build');
+    ;
+const public_path   = '/build';
 
-   devServer: {
-      inline: true,
-      port: 8080
-   },
+const js_path       = path.join(assets_path, '');
 
-   module: {
-      loaders: [
-         {
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
+Encore
+// empty the outputPath dir before each build
+//.cleanupOutputBeforeBuild()
 
-            query: {
-               presets: ['env', 'react']
-            }
-         }
-      ]
-   }
-}
+// directory where all compiled assets will be stored
+    .setOutputPath(output_path)
 
+    // what's the public path to this directory (relative to your project's document root dir)
+    .setPublicPath(public_path)
+
+    // will output as build/app.js
+    .addEntry('app', path.join(js_path, '/index.js'))
+
+
+    .enableSourceMaps(Encore.isProduction())
+
+    // create hashed filenames (e.g. app.abc123.css)
+    .enableVersioning(false)
+    .enableReactPreset()
+;
+
+let config = Encore.getWebpackConfig();
+config.watchOptions = { poll: true, ignored: /node_modules/ };
+
+// export the final configuration
 module.exports = config;
